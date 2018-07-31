803e4515ff858b88c60fedc1c8c534bc8c02fb9d
langer <newChri99>
2018-07-31 10:27:06 +0200

    fix reporting

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,14 +17,18 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        compile('com.lowagie:itext:2.1.7')
+        compile('com.lowagie:itext:2.1.7') {
+            excludes 'groovy-all','org.bouncycastle:bcprov-jdk15on','org.bouncycastle:bcmail-jdk15on',"bouncycastle:bcmail-jdk14:138", "org.bouncycastle:bctsp-jdk14", "bouncycastle:bcprov-jdk14:138", "org.bouncycastle:bcprov-jdk14:1.38"
+        }
 
         compile('net.sf.jasperreports:jasperreports:6.6.0') {
             excludes 'antlr', 'commons-logging',
                     'ant', 'mondrian', 'commons-javaflow','barbecue', 'xml-apis-ext','xml-apis', 'xalan', 'groovy-all', 'hibernate', 'saaj-api', 'servlet-api',
                     'xercesImpl','xmlParserAPIs','spring-core','bsh', 'spring-beans', 'jaxen', 'barcode4j','batik-svg-dom','batik-xml','batik-awt-util','batik-dom',
                     'batik-css','batik-gvt','batik-script', 'batik-svggen','batik-util','batik-bridge','persistence-api','jdtcore','bcmail-jdk16','bcprov-jdk16','bctsp-jdk16',
-                    'bcmail-jdk14','bcprov-jdk14','bctsp-jdk14','xmlbeans', 'olap4j','lucene-core', 'lucene-analyzers-common', 'lucene-queryparser', 'xml-apis','bouncycastle:bcmail-jdk14', "bouncycastle:bcprov-jdk14", "org.bouncycastle:bctsp-jdk14", "org.bouncycastle:bcprov-jdk14"
+                    'bcmail-jdk14','bcprov-jdk14','bctsp-jdk14','xmlbeans', 'olap4j','lucene-core', 'lucene-analyzers-common', 'lucene-queryparser', 'xml-apis','bouncycastle:bcmail-jdk14', "bouncycastle:bcprov-jdk14", "org.bouncycastle:bctsp-jdk14", "org.bouncycastle:bcprov-jdk14",
+                    //'castor-xml', 'ecj', 'icu4j'
+                    'commons-beanutils', 'commons-digester'
         }
 
         compile('org.apache.poi:poi:3.10-FINAL')
@@ -35,7 +39,8 @@ grails.project.dependency.resolution = {
             export = false
         }
 
-        test(":hibernate4:4.3.6.1") {
+        test(":hibernate:3.6.10.19") {
+//        test(":hibernate4:4.3.6.1") {
             export = false
         }
         test(":tomcat:8.0.22") {
